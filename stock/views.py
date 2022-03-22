8561fc4db62d40991deb58efad0cd392b93105a9
faleye-jide <Faleyejide@gmail.com>
2022-03-22 13:21:50 +0000

    added an html form that accepts user inout and passes to to a page

--- stock/views.py
+++ stock/views.py
@@ -1,9 +1,20 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-# from .forms import TickerForm
+from .forms import TickerForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def home(request):
-    pass
-    # form = TickerForm()
-    # return render(request, 'stock/home.html', {'form':form})
+    if request.method == 'POST':
+        form = TickerForm(request.POST)
+        if form.is_valid():
+            ticker = request.POST['ticker']
+            return HttpResponseRedirect(ticker)
+    else:
+        form = TickerForm()
+    return render(request, 'stock/home.html', {'form':form})
+
+def ticker(request, ticker_id):
+    context = {}
+    context['ticker'] = ticker_id
+    return render(request, 'stock/ticker.html', context)
